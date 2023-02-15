ec2aeb19c51900cdc7f9926320aec2f1bc716568
xkcm <42122011+xkcm@users.noreply.github.com>
2023-02-15 15:47:09 +0100

    [KP-1097] Implement live logging

--- helpers.js
+++ helpers.js
@@ -3,8 +3,6 @@ const childProcess = require("child_process");
 const { access } = require("fs/promises");
 const fs = require("fs");
 
-const exec = util.promisify(childProcess.exec);
-
 async function assertPathExistence(path) {
   try {
     await access(path, fs.constants.F_OK);
@@ -13,7 +11,56 @@ async function assertPathExistence(path) {
   }
 }
 
+async function asyncExec(params) {
+  const {
+    command,
+    onProgressFn,
+    options = {},
+  } = params;
+
+  let childProcessError;
+  let childProcessInstance;
+  try {
+    childProcessInstance = childProcess.exec(command, options);
+  } catch (error) {
+    return { error };
+  }
+
+  const outputChunks = [];
+
+  childProcessInstance.stdout.on("data", (data) => {
+    outputChunks.push({ type: "stdout", data });
+
+    onProgressFn?.(data);
+  });
+  childProcessInstance.stderr.on("data", (data) => {
+    outputChunks.push({ type: "stderr", data });
+
+    onProgressFn?.(data);
+  });
+  childProcessInstance.on("error", (error) => {
+    childProcessError = error;
+  });
+
+  try {
+    await util.promisify(childProcessInstance.on.bind(childProcessInstance))("close");
+  } catch (error) {
+    childProcessError = error;
+  }
+
+  const outputObject = outputChunks.reduce((acc, cur) => ({
+    ...acc,
+    [cur.type]: `${acc[cur.type]}\n${cur.data.toString()}`,
+  }), { stdout: "", stderr: "" });
+
+  if (childProcessError) {
+    outputObject.error = childProcessError;
+  }
+
+  return outputObject;
+}
+
 module.exports = {
   assertPathExistence,
-  exec,
+  asyncExec,
 };
